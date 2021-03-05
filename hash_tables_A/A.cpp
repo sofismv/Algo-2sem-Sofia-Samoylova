5d1565cd55324af50a5d82651419528ed7bdcf9e
sofia samoylova <samoilova.se@phystech.edu>
2021-03-06 00:54:26 +0300

    Made some changes

--- hash_tables_A/A.cpp
+++ hash_tables_A/A.cpp
@@ -1,70 +1,76 @@
-#include <list>
 #include <vector>
 #include <iostream>
 
 struct Query {
   std::string str;
   int value;
-  Query(const std::string& str, int value): str(str), value(value) {};
+  Query(const std::string &str, int value) : str(str), value(value) {};
 };
 
 class HashTable {
-  uint64_t size;
-  std::vector<std::vector<Query>> hash_table;
  public:
-  HashTable(uint64_t size) :size(size) {
-    hash_table.resize(size);
-  };
-  uint64_t HashFunction (const std::string &s) {
-    uint64_t hash = 0;
-    for (char i : s) {
-      hash = 31 * hash + i;
-    }
-    hash %= size;
-    return hash;
+  int Incrementator(const std::string &s, int number);
+  explicit HashTable(uint64_t size) : size_(size) {
+    hash_table_.resize(size);
   }
-  void Insert(const std::string &s, int number) {
-    uint64_t hash = HashFunction(s);
-    hash_table[hash].emplace_back(Query(s, number));
-  }
-  bool Find(const std::string &s) {
-    uint64_t hash = HashFunction(s);
-    for (const auto& i : hash_table[hash]) {
-      if (i.str == s) {
-        return true;
-      }
-    }
-    return false;
-  }
-  int Increment(const std::string &s, int number) {
-    uint64_t hash = HashFunction(s);
-    for (int i = 0; i < hash_table[hash].size(); ++i) {
-      if (hash_table[hash][i].str == s) {
-        hash_table[hash][i].value += number;
-        return hash_table[hash][i].value;
-      }
-    }
-    return 0;
-  }
-  void Queries (const std::string &s, int number) {
-    if (Find(s)) {
-      std::cout << Increment(s, number) << '\n';
-    } else {
-      Insert(s, number);
-      std::cout << number << '\n';
-    }
-  }
+ private:
+  uint64_t HashFunction(const std::string &s);
+  void Insert(const std::string &s, int number);
+  bool Find(const std::string &s);
+  int Increment(const std::string &s, int number);
 
+ private:
+  uint64_t size_;
+  std::vector<std::vector<Query>> hash_table_;
 };
+
+int HashTable::Incrementator(const std::string &s, int number) {
+  if (Find(s)) {
+    return Increment(s, number);
+  } else {
+    Insert(s, number);
+    return number;
+  }
+}
+uint64_t HashTable::HashFunction(const std::string &s) {
+  uint64_t hash = 0;
+  for (char i : s) {
+    hash = 31 * hash + i;
+  }
+  hash %= size_;
+  return hash;
+}
+void HashTable::Insert(const std::string &s, int number) {
+  uint64_t hash = HashFunction(s);
+  hash_table_[hash].emplace_back(Query(s, number));
+}
+bool HashTable::Find(const std::string &s) {
+  uint64_t hash = HashFunction(s);
+  for (const auto &i : hash_table_[hash]) {
+    if (i.str == s) {
+      return true;
+    }
+  }
+  return false;
+}
+int HashTable::Increment(const std::string &s, int number) {
+  uint64_t hash = HashFunction(s);
+  for (int i = 0; i < hash_table_[hash].size(); ++i) {
+    if (hash_table_[hash][i].str == s) {
+      hash_table_[hash][i].value += number;
+      return hash_table_[hash][i].value;
+    }
+  }
+  return 0;
+}
+
 int main() {
   std::string s;
   int n;
   HashTable table(1000);
   while (std::cin >> s) {
     std::cin >> n;
-    table.Queries(s, n);
+    std::cout << table.Incrementator(s, n) << '\n';
   }
-    return 0;
+  return 0;
 }
-
-
