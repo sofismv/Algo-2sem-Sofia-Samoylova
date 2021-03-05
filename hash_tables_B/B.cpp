c00d27eb0e8b3994763f7039a86e854c36087aaa
sofia samoylova <samoilova.se@phystech.edu>
2021-03-06 01:09:11 +0300

    made some changes to B

--- hash_tables_B/B.cpp
+++ hash_tables_B/B.cpp
@@ -1,57 +1,58 @@
-#include <iostream>
 #include <vector>
 #include <cmath>
-// first - key, second - position
+#include <iostream>
+
+struct Element {
+  int key;
+  int value;
+  Element(int key, int value) : key(key), value(value) {};
+};
 
 class HashTable {
-  uint64_t size;
-  std::vector<std::pair<int, int>> hash_table;
  public:
-  HashTable(uint64_t size) : size(size) {
-    hash_table.resize(size);
+  explicit HashTable(uint64_t size) : size_(size) {
     for (int i = 0; i < size; ++i) {
-      hash_table[i] = std::make_pair(0, 0);
+      hash_table_.emplace_back(0, 0);
     }
   };
-  uint64_t HashFunction(int key) {
-    return ((key % size) * 31) % size;
-  }
-  int Insert(int key) {
-    int q = 1;
-    int pos = HashFunction(key);
-    while (pos < size && hash_table[pos].first != 0 && hash_table[pos].first != key) {
-      pos += q;
-      pos %= size;
-    }
-    if (hash_table[pos].first == key) {
-      return pos;
-    }
-    hash_table[pos] = std::make_pair(key, key);
-    return pos;
-  }
-  int Find(int key) {
-    int pos = 0;
-    int q = HashFunction(key);
-    while (pos < size && hash_table[pos].first != key) {
-      pos += q;
-    }
-    if (pos >= size) {
-      return -1;
-    }
-    return pos;
-  }
-  int Swap(int a, int b) {
-    int a_pos = Insert(a);
-    int b_pos = Insert(b);
-    int k = abs(hash_table[a_pos].second - hash_table[b_pos].second);
-    std::swap(hash_table[a_pos].second, hash_table[b_pos].second);
-    return k;
-  }
+  int Swap(int a, int b);
+ private:
+  uint64_t HashFunction(int key);
+  int Insert(int key);
+
+ private:
+  uint64_t size_;
+  std::vector<Element> hash_table_;
 };
+
+int HashTable::Swap(int a, int b) {
+  int a_pos = Insert(a);
+  int b_pos = Insert(b);
+  int k = abs(hash_table_[a_pos].value - hash_table_[b_pos].value);
+  std::swap(hash_table_[a_pos].value, hash_table_[b_pos].value);
+  return k;
+}
+uint64_t HashTable::HashFunction(int key) {
+  return ((key % size_) * 31) % size_;
+}
+int HashTable::Insert(int key) {
+  int q = 1;
+  int pos = HashFunction(key);
+  while (pos < size_ && hash_table_[pos].key != 0 && hash_table_[pos].key != key) {
+    pos += q;
+    pos %= size_;
+  }
+  if (hash_table_[pos].key == key) {
+    return pos;
+  }
+  hash_table_[pos] = Element(key, key);
+  return pos;
+}
+
 int main() {
   std::ios_base::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
   int n, x, y;
   std::cin >> n;
   HashTable table(500000);
