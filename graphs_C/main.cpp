3a9176012e5a8769c1626c1709dc6c9d3ad6b9e1
sofia samoylova <samoilova.se@phystech.edu>
2021-04-17 14:09:49 +0300

    changing graphs

--- graphs_C/main.cpp
+++ graphs_C/main.cpp
@@ -2,22 +2,20 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<std::vector<int>> g;
-std::vector<int> tin;
-std::vector<int> tout;
-std::vector<int> parent;
-int n;
-int cycle_vertex;
-int timer = 0;
-std::vector<std::string> color;
-
-bool dfs(int v) {
+bool dfs(std::vector<int> &tin,
+         std::vector<int> &tout,
+         std::vector<std::string> &color,
+         int &timer,
+         int &cycle_vertex,
+         std::vector<int> &parent,
+         const std::vector<std::vector<int>> &g,
+         int v) {
   tin[v] = timer++;
   color[v] = "GRAY";
   for (int to : g[v]) {
     if (color[to] == "WHITE") {
       parent[to] = v;
-      if (dfs(to)) {
+      if (dfs(tin, tout, color, timer, cycle_vertex, parent, g, to)) {
         return true;
       }
     }
@@ -32,34 +30,37 @@ bool dfs(int v) {
   return false;
 }
 
-int main() {
-  int m, u, v;
-  std::cin >> n >> m;
-  g.resize(n);
-  tin.resize(n);
-  tout.resize(n);
-  parent.resize(n);
-  color.resize(n, "WHITE");
+void print_cycle(const std::vector<int> &parent, int cycle_vertex) {
   std::vector<int> ans;
+  int vertex = parent[cycle_vertex];
+  while (vertex != cycle_vertex) {
+    ans.push_back(vertex + 1);
+    vertex = parent[vertex];
+  }
+  ans.push_back(cycle_vertex + 1);
+  std::reverse(ans.begin(), ans.end());
+  for (auto i : ans) {
+    std::cout << i << ' ';
+  }
+}
+
+int main() {
+  int m, u, v, n, cycle_vertex, timer = 0;
+  std::cin >> n >> m;
+  std::vector<std::vector<int>> g(n);
+  std::vector<int> tin(n);
+  std::vector<int> tout(n);
+  std::vector<int> parent(n);
+  std::vector<std::string> color(n, "WHITE");
   for (int i = 0; i < m; ++i) {
     std::cin >> u >> v;
-    --u; --v;
-    g[u].push_back(v);
+    g[--u].push_back(--v);
   }
   for (int v = 0; v < n; ++v) {
     if (color[v] == "WHITE") {
-      if (dfs(v)) {
+      if (dfs(tin, tout, color, timer, cycle_vertex, parent, g, v)) {
         std::cout << "YES\n";
-        int vertex = parent[cycle_vertex];
-        while (vertex != cycle_vertex) {
-          ans.push_back(vertex + 1);
-          vertex = parent[vertex];
-        }
-        ans.push_back(cycle_vertex + 1);
-        std::reverse(ans.begin(), ans.end());
-        for (auto i : ans){
-          std::cout << i << ' ';
-        }
+        print_cycle(parent, cycle_vertex);
         return 0;
       }
     }
