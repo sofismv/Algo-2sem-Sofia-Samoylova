b92c3953412af8a8bd21e36f6cb13cb99b9141b3
sofia samoylova <samoilova.se@phystech.edu>
2021-04-17 11:29:19 +0300

    changing graphs

--- graphs_B/main.cpp
+++ graphs_B/main.cpp
@@ -1,37 +1,34 @@
 #include <iostream>
 #include <vector>
 
-std::vector<std::vector<int>> g;
-std::vector<int> tin;
-std::vector<int> tout;
-int n;
-int timer = 0;
-std::vector<int> used;
-
-void dfs(int v) {
+void dfs(std::vector<int> &tin,
+         std::vector<int> &tout,
+         int &timer,
+         std::vector<int> &used,
+         const std::vector<std::vector<int>> &g,
+         int v) {
   tin[v] = timer++;
   used[v] = 1;
   for (int to : g[v]) {
     if (used[to]) {
       continue;
     }
-    dfs(to);
+    dfs(tin, tout, timer, used, g, to);
   }
   tout[v] = timer++;
 }
 
-bool is_parent (int a, int b) {
+bool is_parent(const std::vector<int> &tin, const std::vector<int> &tout, int a, int b) {
   return tout[a] > tout[b] && tin[a] < tin[b];
 }
 
-
 int main() {
-  int n, parent, m, a, b, root;
+  int n, parent, m, a, b, root, timer = 0;
   std::cin >> n;
-  g.resize(n);
-  tin.resize(n);
-  tout.resize(n);
-  used.resize(n);
+  std::vector<std::vector<int>> g(n);
+  std::vector<int> tin(n);
+  std::vector<int> tout(n);
+  std::vector<int> used(n);
   for (int i = 0; i < n; ++i) {
     std::cin >> parent;
     if (parent == 0) {
@@ -41,12 +38,13 @@ int main() {
     parent--;
     g[parent].push_back(i);
   }
-  dfs(root);
+  dfs(tin, tout, timer, used, g, root);
   std::cin >> m;
   for (int i = 0; i < m; ++i) {
     std::cin >> a >> b;
-    --a; --b;
-    std::cout << is_parent(a, b) << '\n';
+    --a;
+    --b;
+    std::cout << is_parent(tin, tout, a, b) << '\n';
   }
   return 0;
 }
